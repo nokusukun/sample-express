5dff4a66ac1cd9cec5d97bdd913492cd37e55622
Natsuko Higashikuni <nokusukun@yahoo.co.jp>
2020-10-09 05:25:41 +0800

    Reconfigured

--- index.js
+++ index.js
@@ -7,8 +7,10 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-const host = process.env.HOST
+const _host = process.env.APP_HOST
+const host = _host.split(":")[0]
+const port = Number(_host.split(":")[1])
 
-app.listen(host, () => {
-    console.log(`Example app listening at ${host}`)
+app.listen(port, host, () => {
+    console.log(`Example app listening at ${host}:${port}`)
 })
